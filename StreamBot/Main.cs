bc509ec0e7ecbde6d91442cc3d4fbfafba6c5aa6
Tomas Drbota <argoneuscze@gmail.com>
2012-08-10 19:13:20 +0200

    added exception logging

--- StreamBot/Main.cs
+++ StreamBot/Main.cs
@@ -4,9 +4,16 @@ namespace StreamBot
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
-		{
-			IRCBot.Bot.Start();
+		public static void Main(string[] args)
+        {
+            try
+            {
+                IRCBot.Bot.Start();
+            }
+            catch (Exception e)
+            {
+                IRCBot.Log.AddErrorMessage(e.Message);
+            }
 		}
 	}
 }
