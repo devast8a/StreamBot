902f02e2c0e998e1693e7c69f6720083d0bd7e0f
devast8a <devast8a@whitefirex.com>
2012-08-22 00:01:33 +0930

    Shift+Q now exits console. Slight changes to file saving.

--- StreamBot/Main.cs
+++ StreamBot/Main.cs
@@ -1,18 +1,28 @@
 using System;
 using System.Threading;
 using StreamBot.IRCBot;
+using StreamBot.IRCBot.Commands;
+using log4net;
 using log4net.Config;
 
 namespace StreamBot
 {
     internal class Program
     {
-        private static readonly TimeSpan SaveInterval = TimeSpan.FromSeconds(300);
-        private static readonly Timer SettingsTimer = new Timer(SaveSettings, SettingsTimer, SaveInterval, SaveInterval);
+        private static readonly TimeSpan SaveInterval = TimeSpan.FromSeconds(30);
+        private static Timer _settingsTimer;
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(UpdateStream));
 
         private static void SaveSettings(object state)
         {
             var settings = (SettingsFile)state;
+            
+            if(settings == null)
+            {
+                Logger.Fatal("Settings could not be saved. settings is null.");
+                return;
+            }
+
             settings.Save();
         }
 
@@ -21,9 +31,11 @@ namespace StreamBot
             // Configure log4net from app.config
             XmlConfigurator.Configure();
 
-            var settings = new SettingsFile("Settings.xml");
 
-            Console.WriteLine("Press <ENTER> to quit.");
+            var settings = new SettingsFile("Settings.xml");
+            _settingsTimer = new Timer(SaveSettings, settings, SaveInterval, SaveInterval);
+
+            Console.WriteLine("Press Shift+Q to quit.");
 
             foreach (var instance in settings.Instances)
             {
@@ -31,7 +43,15 @@ namespace StreamBot
                 ThreadPool.QueueUserWorkItem(a => bot.Connect());
             }
 
-            Console.ReadLine();
+            while(true)
+            {
+                var key = Console.ReadKey(true);
+
+                if(key.Modifiers.HasFlag(ConsoleModifiers.Shift) && key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+            }
 
             settings.Save();
         }
