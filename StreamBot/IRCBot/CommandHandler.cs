10507ad4964b9e08ef5cce1e88d8b5014a173d64
J.C. Moyer <jmoyer1992@gmail.com>
2012-08-20 23:55:53 -0400

    Fixing formatting

--- StreamBot/IRCBot/CommandHandler.cs
+++ StreamBot/IRCBot/CommandHandler.cs
@@ -3,7 +3,7 @@ using StreamBot.IRCBot.Commands;
 
 namespace StreamBot.IRCBot
 {
-    class CommandHandler
+    internal class CommandHandler
     {
         private readonly Dictionary<string, ICommand> _commands = new Dictionary<string, ICommand>();
 
@@ -11,18 +11,20 @@ namespace StreamBot.IRCBot
         {
             _commands.Add(text, command);
         }
- 
+
         public string ParseCommand(string sender, Permission permission, string message)
         {
             int firstSpace = message.IndexOf(' ');
 
             var args = new CommandArgs();
 
-            if(firstSpace == -1){
+            if (firstSpace == -1)
+            {
                 args.Name = message;
                 args.Full = "";
                 args.Args = new string[0];
-            }else
+            }
+            else
             {
                 args.Name = message.Substring(0, firstSpace);
                 args.Full = message.Substring(firstSpace + 1).Trim();
@@ -31,7 +33,7 @@ namespace StreamBot.IRCBot
 
             ICommand command;
 
-            if(_commands.TryGetValue(args.Name, out command))
+            if (_commands.TryGetValue(args.Name, out command))
             {
                 return command.Parse(sender, permission, args);
             }
@@ -40,4 +42,3 @@ namespace StreamBot.IRCBot
         }
     }
 }
-
