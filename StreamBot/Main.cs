8f250e2fdc013a37d765862dc86096a813815a33
J.C. Moyer <jmoyer1992@gmail.com>
2012-08-20 20:00:19 -0400

    Store timer reference to protect against GC

--- StreamBot/Main.cs
+++ StreamBot/Main.cs
@@ -7,6 +7,7 @@ namespace StreamBot
     class MainClass
     {
         private static readonly TimeSpan SaveInterval = TimeSpan.FromSeconds(300);
+        private static readonly Timer SettingsTimer = new Timer(SaveSettings, SettingsTimer, SaveInterval, SaveInterval);
 
         private static void SaveSettings(object state)
         {
@@ -17,7 +18,6 @@ namespace StreamBot
         public static void Main(string[] args)
         {
             SettingsFile settings = new SettingsFile("Settings.xml");
-            new Timer(SaveSettings, settings, SaveInterval, SaveInterval);
 
             Console.WriteLine("Press <ENTER> to quit.");
 
